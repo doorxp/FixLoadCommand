5612ab5d9748b8e691514e0a05b48fecdf90f5b9
doorxp <doorxp@msn.com>
2017-07-31 15:20:49 +0800

    Initial Commit

---
+++ mh/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mh
+//
+//  Created by doorxp on 2017/7/31.
+//  Copyright © 2017年 doorxp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
